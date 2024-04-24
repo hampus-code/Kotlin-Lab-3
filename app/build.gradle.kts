a3d75879350b90d78a1934b09d72db3aedd17ad4
hampus-code <hampusss96@gmail.com>
2024-04-24 13:49:36 +0200

    Also added dependency for Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
 
