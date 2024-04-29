aaaed5ea40ca5ede8842151918914d734c70df39
hampus-code <hampusss96@gmail.com>
2024-04-29 16:44:49 +0200

    Added TextButton composable on LoginScreen composable

--- app/src/main/java/com/hampuscode/lab_3/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/hampuscode/lab_3/ui/screens/LoginScreen.kt
@@ -1,5 +1,8 @@
 package com.hampuscode.lab_3.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,11 +16,16 @@ import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,8 +57,12 @@ fun LoginScreen() {
                 trailingIcon = { Icon(imageVector = Icons.Outlined.Clear, contentDescription = "")},
                 value = "Username", onValueChange = {},
                 label = { Text(text = "Username")},
-                modifier = Modifier.padding(vertical = 10.dp)
+                modifier = Modifier.padding(vertical = 10.dp),
+
+
             )
+
+            //TODO - Make the clear icon on password and eye instead. And when i hold on the eye you can see the password
 
             OutlinedTextField(
                 leadingIcon = { Icon(imageVector = Icons.Outlined.Lock, contentDescription = "")},
@@ -58,8 +70,13 @@ fun LoginScreen() {
                 value = "Password", onValueChange = {},
                 label = { Text(text = "Password")},
                 visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.padding(vertical = 10.dp)
+
             )
+
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Don't have an account? Register here",
+                    textDecoration = TextDecoration.Underline)
+            }
 
             Button(onClick = { /*TODO*/ }) {
                 Text(text = "Login")
--- app/src/main/java/com/hampuscode/lab_3/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/hampuscode/lab_3/ui/screens/RegisterScreen.kt
@@ -12,10 +12,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
